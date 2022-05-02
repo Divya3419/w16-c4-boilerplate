f9a9a5ef0cde872fcee34649338f72af266d54f7
Divya <99958902+Divya3419@users.noreply.github.com>
2022-05-02 14:58:56 +0530

    done

---
+++ test/src/Routes/MainRoutes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes,Route} from "react-router";
+
+import Home from "../components/Home"
+import About from "../components/About"
+const MainRoutes = () => {
+  return <>
+
+  <Routes>
+    <Route path="/"  element={<Home/>} />
+    <Route path ="/about" element={<About/>} />
+    <Route path="/products" element={<products/>} />
+    <Route path ="/products/men" element={<productCard>} />
+    <Route/>
+    <Route/>
+    <Route/>
+  </Routes>
+  </>;
+};
+export { MainRoutes };
